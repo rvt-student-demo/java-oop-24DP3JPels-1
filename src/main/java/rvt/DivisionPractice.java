ff0abfe6b6f1ad5177489401ce3199dc7eabc53c
24DP3JPels <A240529JP@rvt.lv>
2026-01-22 14:04:03 +0200

    Division Practice pabeigts

---
+++ src/main/java/rvt/DivisionPractice.java
@@ -0,0 +1,48 @@
+package rvt;
+import java.util.Scanner;
+
+public class DivisionPractice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input;
+        int numerator, divisor, result;
+
+        while (true) {
+            System.out.print("Enter the numerator: ");
+            input = scan.next();
+
+            
+            if (input.toLowerCase().charAt(0) == 'q') {
+                break;
+            }
+
+            try {
+                numerator = Integer.parseInt(input);
+
+                System.out.print("Enter the divisor: ");
+                divisor = scan.nextInt();
+
+                
+                if (divisor == 0) {
+                    throw new ArithmeticException("zero");
+                }
+
+                result = numerator / divisor;
+                System.out.println(numerator + " / " + divisor + " is " + result);
+
+            } catch (NumberFormatException e) {
+                
+                System.out.println("Ievadiji nepareizus datus.");
+                System.out.println("Megini velreiz.");
+                scan.nextLine(); 
+            } catch (ArithmeticException e) {
+
+                System.out.println("You can't divide by zero.");
+            }
+            
+            System.out.println(); 
+        }
+        
+        scan.close();
+    }
+}
