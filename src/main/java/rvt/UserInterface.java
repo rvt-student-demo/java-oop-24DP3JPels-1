38ab3f3d42f8ac2329839e8efc5fe8b196a0dc59
24DP3JPels <a240529jp@rvt.lv>
2026-01-29 10:51:18 +0000

    Java Mooc 6.2 uzd

---
+++ src/main/java/rvt/UserInterface.java
@@ -0,0 +1,38 @@
+package rvt;
+import java.util.Scanner;
+
+public class UserInterface {
+    private TodoList todoList;
+    private Scanner scanner;
+
+    public UserInterface(TodoList todoList, Scanner scanner) {
+        this.todoList = todoList;
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("stop")) {
+                break;
+            }
+
+            if (command.equals("add")) {
+                System.out.print("To add: ");
+                String task = scanner.nextLine();
+                this.todoList.add(task);
+
+            } else if (command.equals("list")) {
+                this.todoList.print();
+
+            } else if (command.equals("remove")) {
+                System.out.print("Which one is removed? ");
+                
+                int id = Integer.parseInt(scanner.nextLine());
+                this.todoList.remove(id);
+            }
+        }
+    }
+}
